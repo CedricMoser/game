1d0d444e307a754e168e94202f9ba6aba392d82e
nioxx <eppletobias@web.de>
2017-12-06 21:08:08 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        int a =1;
+
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
-        test123
+        System.out.print(a);
     }
 }
